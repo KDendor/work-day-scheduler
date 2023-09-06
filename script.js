56768bd52aac8529c061dc51e0a0548e85975542
Caitlyn Erwin <caitlynelizabetherwin@gmail.com>
2023-09-06 15:34:17 -0400

    modified js

--- script.js
+++ script.js
@@ -1,55 +1,62 @@
 $(document).ready(function() {
+    $("currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+    //save button
+    $(".saveBtn").on("click", function () {
+        console.log(this);
 
-}
-
-$(document).ready(function() {
-    addEventListener("click", (event) => {});
-    onclick = (event) => {};
-
-    var input = document.getElementById("saveButton");
-    localStorage.setItem("saveButton", input.val());
-
-    const dayjs = require('dayjs')
-    dayjs().format()
-
-    require('dayjs/locale/de')
-    dayjs.locale('de') 
-    dayjs().locale('de').format() 
-
-    if (timeBlock < currentTime) { 
-        then (apply id="past") 
-    } else if (timeBlock = currentTime)  {
-        then (apply id="present")
-    } else if (timeBlock > currentTime) {
-        then (apply id="future")
-    }
-        
-
+    var text = $(this)
+    var time = $(this).attr("id");
+    localStorage.setItem(time,text);
 });
 
+$("#hour-9 .description").val(localStorage.getItem("hour9"));
+$("#hour-10 .description").val(localStorage.getItem("hour10"));
+$("#hour-11 .description").val(localStorage.getItem("hour11"));
+$("#hour-12 .description").val(localStorage.getItem("hour12"));
+$("#hour-1 .description").val(localStorage.getItem("hour1"));
+$("#hour-2 .description").val(localStorage.getItem("hour2"));
+$("#hour-3 .description").val(localStorage.getItem("hour3"));   
+$("#hour-4 .description").val(localStorage.getItem("hour4"));
+$("#hour-5 .description").val(localStorage.getItem("hour5"));
 
+function hourTracker() {
+    var currentHour = moment().hour(); //moment.js
 
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () { 
+    $(".time-block").each(function () {
+        var hour = parseInt($(this).attr("id").split("hour")[1]);
+        console.log( blockHour, currentHour)
 
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });
+        if (blockHour < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("present");
+            $(this).removeClass("future"); 
+        }
+        else if (blockHour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeCLass("future");
+        }
+        else {
+            $(this).removeClass("past")
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    }
+    hourTracker();
+})
+
+// const dayjs = require('dayjs')
+//     dayjs().format()
+
+//     require('dayjs/locale/de')
+//     dayjs.locale('de') 
+//     dayjs().locale('de').format() 
+
+//     if (timeBlock < currentTime) { 
+//         then (apply id="past") 
+//     } else if (timeBlock = currentTime)  {
+//         then (apply id="present")
+//     } else if (timeBlock > currentTime) {
+//         then (apply id="future")
+//     }
+// });
